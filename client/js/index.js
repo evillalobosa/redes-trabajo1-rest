1100120fab627575349951c49c59bb183fa50165
enrique villalobos andrade <enrique.lnx@gmail.com>
2022-04-29 13:04:12 -0400

    Updating interface
    And adding favicon

--- client/js/index.js
+++ client/js/index.js
@@ -1,14 +1,54 @@
 function getRut() {
-    var rut  = document.getElementById("rutField").value;
-    var rutd = document.getElementById("rutdField").value;
+    var rut  = document.getElementById("rutField").value; // String type
+    var validador = document.getElementById("rutdField").value; // String type
 
-    console.log("Boton RUT: " + rut + "-" + rutd);
-    return true;
+    // VALIDATIONS
+    if (rut.length === 8 && validador.length === 1) {
+        console.log("Rut ingresado maomeno");
+
+        // Show correct msg
+        var error_result = document.getElementById("rut-error");
+        error_result.classList.add("hide");
+
+        var correct_result = document.getElementById("rut-correct");
+        correct_result.classList.remove("hide");
+    }
+    else {
+        // TODO: add msg to logs
+        console.log("Rut mal ingresado");
+
+        // Show error msg
+        var correct_result = document.getElementById("rut-correct");
+        correct_result.classList.add("hide");
+
+        var error_result = document.getElementById("rut-error");
+        error_result.classList.remove("hide");
+    }
 }
 
 function getNombre(){
     var nombre = document.getElementById("nombreField").value;
 
-    console.log("Boton Nombre: " + nombre);
-    return "nombre1";
+    // VALIDATIONS
+    if (nombre) {
+        console.log("Nombre ingresado maomeno")
+
+        // Show correct msg
+        var error_result = document.getElementById("nombre-error");
+        error_result.classList.add("hide");
+
+        var correct_result = document.getElementById("nombre-correct");
+        correct_result.classList.remove("hide");
+    }
+    else {
+        // TODO: add msg to logs
+        console.log("Nombre mal ingresado")
+
+        // Show error msg
+        var correct_result = document.getElementById("nombre-correct");
+        correct_result.classList.add("hide");
+
+        var error_result = document.getElementById("nombre-error");
+        error_result.classList.remove("hide");
+    }
 }
