30daa4f557887c0b1d12888bed2f69a8651f5a7c
enrique villalobos andrade <enrique.lnx@gmail.com>
2022-04-30 20:30:09 -0400

    updating js functions

--- client/js/index.js
+++ client/js/index.js
@@ -1,74 +1,135 @@
+// ============================================================== //
+// ===================== RUT ==================================== //
+// ============================================================== //
+
+function showRutResult(status) {
+    if (status) {
+        // Show correct msg
+        var rut_error_html = document.getElementById("rut-error");
+        rut_error_html.classList.add("hide");
+
+        var rut_correct_html = document.getElementById("rut-correct");
+        rut_correct_html.classList.remove("hide");
+    }
+    else {
+        // Show error msg
+        var rut_correct_html = document.getElementById("rut-correct");
+        rut_correct_html.classList.add("hide");
+
+        var rut_error_html = document.getElementById("rut-error");
+        rut_error_html.classList.remove("hide");
+    }
+}
+
 function getRut() {
     var rut  = document.getElementById("rutField").value; // String type
     var validador = document.getElementById("rutdField").value; // String type
 
     // VALIDATIONS
+    // TODO: check if rut and validators are int numbers and not letters or whatever
     if (rut.length === 8 && validador.length === 1) {
-        console.log("Rut ingresado maomeno");
+        console.log("===> Rut ingresado +-");
 
         // Send request
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/products", true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify({
-            rut: Number(rut),
-            validador: Number(validador)
-        }));
-        xhr.onload = function() {
-            console.log("--------------- recibido?")
-            console.log(this.responseText);
-            var data = JSON.parse(this.responseText);
-            console.log(data);
-            console.log("-----------------")
-        }
+        fetch('http://localhost:8080/products', {
+            method:'POST',
+            body:JSON.stringify({
+                rut: Number(rut),
+                validador:Number(validador)
+            }),
+            headers:{
+                "Content-Type":"application/json; charset=UTF-8"
+            }
+        })
+        .then((resp) => resp.json())
+        .then(function(data){
+            console.log(data.results)
 
-        // Show correct msg
-        var error_result = document.getElementById("rut-error");
-        error_result.classList.add("hide");
+            if (data.results){
+                showRutResult(true)
+            }
+            else {
+                showRutResult(false)
+            }
 
-        var correct_result = document.getElementById("rut-correct");
-        correct_result.classList.remove("hide");
+        })
+        .catch(function() {
+            console.log("error en la peticion del rut")
+
+        })
     }
     else {
         // TODO: add msg to logs
-        console.log("Rut mal ingresado");
+        console.log("===> Rut mal ingresado");
 
-        // Show error msg
-        var correct_result = document.getElementById("rut-correct");
-        correct_result.classList.add("hide");
-
-        var error_result = document.getElementById("rut-error");
-        error_result.classList.remove("hide");
+        showRutResult(false)
     }
 }
 
 
 // ============================================================== //
+// ===================== NOMBRE ================================= //
+// ============================================================== //
 
+
+function showNombreResult(status) {
+    if (status) {
+        // Show correct msg
+        var nombre_error_html = document.getElementById("nombre-error");
+        nombre_error_html.classList.add("hide");
+
+        var nombre_correct_html = document.getElementById("nombre-correct");
+        nombre_correct_html.classList.remove("hide");
+    }
+    else {
+        // Show error msg
+        var nombre_correct_html = document.getElementById("nombre-correct");
+        nombre_correct_html.classList.add("hide");
+
+        var nombre_error_html = document.getElementById("nombre-error");
+        nombre_error_html.classList.remove("hide");
+    }
+}
 
 function getNombre(){
-    var nombre = document.getElementById("nombreField").value;
+    var nombre_completo = document.getElementById("nombreField").value;
 
     // VALIDATIONS
+    // TODO: check if nombre_completo arent numbers
     if (nombre) {
-        console.log("Nombre ingresado maomeno")
+        console.log("===> Nombre ingresado +-")
 
-        // Show correct msg
-        var error_result = document.getElementById("nombre-error");
-        error_result.classList.add("hide");
+        // Send request
+        fetch('http://localhost:8080/products', {
+            method:'POST',
+            body:JSON.stringify({
+                nombres:nombre_completo,
+            }),
+            headers:{
+                "Content-Type":"application/json; charset=UTF-8"
+            }
+        })
+        .then((resp) => resp.json())
+        .then(function(data){
+            console.log(data.results)
 
-        var correct_result = document.getElementById("nombre-correct");
-        correct_result.classList.remove("hide");
+            // var status = data.results[1]
+            // var nombres = data.results[2]
+            // var apellidos = data.results[3]
+
+            // if (status) {}
+
+        })
+        .catch(function() {
+            console.log("error en la peticion del nombre")
+        })
+
+        showNombreResult(true)
     }
     else {
         // TODO: add msg to logs
-        console.log("Nombre mal ingresado")
+        console.log("===> Nombre mal ingresado")
 
-        // Show error msg
-        var correct_result = document.getElementById("nombre-correct");
-        correct_result.classList.add("hide");
-
-        var error_result = document.getElementById("nombre-error");
-        error_result.classList.remove("hide");
+        showNombreResult(false)
     }
 }
